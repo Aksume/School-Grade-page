2b9aa96582fde360b457316093157ba960139f7e
Tsion Gebrehiwot <tsiondanyom@yahoo.com>
2022-05-21 11:45:49 -0500

    feat(school gread page):added Javascript

--- Js/script.js
+++ Js/script.js
@@ -6,14 +6,17 @@ function getGrade() {
 	} else if (20 < score && score <= 25) {
 		result.innerHTML = "You got  'B' Great !";
 	} else if (15 < score && score <= 20) {
-		result.innerHTML = "You got  'c' Aim Higher.";
+		result.innerHTML = "You got 'c'you need to steady.";
 	} else if (10 < score && score <= 15) {
 		result.innerHTML = "You got  'D' you need to steady.";
 	} else if (5 < score && score <= 10) {
-		result.innerHTML = "You got  'E' You have failed .";
+		result.innerHTML = "You got'E' You failed .";
 	} else if (0 < score && score <= 5) {
 		result.innerHTML = "You got 'F' You  failed";
 	} else {
 		window.alert("Invalid score.please enter a numerical greade.");
 	}
 }
+
+// Project Ideas (CRUD APP):- Employee Management app- Book Club/Library App- Pokemon Tracker- Meme generator (add captions to pics)- Recipe App- Gratitude Daily Journal App
+
